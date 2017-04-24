6291d1f7f52615f8e3b4bdce393205a8f795e8d9
Iván <ivanmartinga@gmail.com>
2017-04-24 16:52:13 +0200

    Increase min window size

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../layouts/main_layout.fxml"));
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
